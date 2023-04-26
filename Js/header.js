5ef3aba64de92a70f351a139fb8d1439e956a558
Rodrigo <ezequielcomar86@gmail.com>
2023-04-26 13:44:48 -0300

    Realizacion de scroll animation en sobre mi

--- Js/header.js
+++ Js/header.js
@@ -8,3 +8,18 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
+
+///animation-scroll
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
