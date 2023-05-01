1d7eeaf454204815daf7dc69d4dcb22fecfeab52
Rodrigo <ezequielcomar86@gmail.com>
2023-04-30 22:51:19 -0300

    Integracion de libreria lazy-load

--- Js/header.js
+++ Js/header.js
@@ -23,3 +23,8 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+//Renderizacion de imagenes
+var lazyLoadInstance = new LazyLoad({
+  elements_selector: ".lazy",
+});
